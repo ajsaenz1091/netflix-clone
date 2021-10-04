6551518927049336620c9bc3418f5f1cd5598746
Alberto <ajsaenz1091@gmail.com>
2021-10-04 19:10:09 -0400

    Added scroll functionality to rows with hidden scrollbar

--- src/components/movie-poster/MoviePoster.js
+++ src/components/movie-poster/MoviePoster.js
@@ -4,13 +4,13 @@ import './MoviePoster.css'
 function MoviePoster({poster_path, name, title}) {
     const img_src =`https://image.tmdb.org/t/p/original/${poster_path}` 
     return (
-        <div >
+        <>
             <img
                 className="row-poster" 
                 src={img_src} 
                 alt={name || title}
             />
-        </div>
+        </>
     )
 }
 
